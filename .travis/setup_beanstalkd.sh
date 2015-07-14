fc590b15762677a4f53ff750d51d9ec2244e21b2
Pierre Chapuis <catwell-github@catwell.info>
2015-07-14 18:11:59 +0200

    oops

--- .travis/setup_beanstalkd.sh
+++ .travis/setup_beanstalkd.sh
@@ -7,7 +7,7 @@ src_url="https://github.com/kr/beanstalkd/archive/v${BEANSTALKD_VERSION}.tar.gz"
 
 mkdir -p "$HOME/.local/bin"
 
-curl -L "$src_url" | tar gz
+curl -L "$src_url" | tar xz
 pushd "beanstalkd-${BEANSTALKD_VERSION}"
     make
     mv beanstalkd "$HOME/.local/bin"
